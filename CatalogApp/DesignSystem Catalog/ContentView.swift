7939b568f5b6f19ce3e4dcd90673f1d642b3ead8
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-02-16 15:16:59 +0100

    feat: Init catalog app

---
+++ CatalogApp/DesignSystem Catalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignSystem Catalog
+//
+//  Created by Jordan on 16.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CatalogApp/DesignSystem Catalog/DesignSystem_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystem_CatalogApp.swift
+//  DesignSystem Catalog
+//
+//  Created by Jordan on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystem_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
