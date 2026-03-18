997ec089beab3b0eadf5dae65b7aba7213cebcf2
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-03-18 16:59:53 +0100

    feat: light and dark Color extension

---
+++ Sources/iOSDesignSystem/Color+Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Color+Extensions.swift
+//  iOSDesignSystem
+//
+//  Created by Jordan on 18.03.2026.
+//
+
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
+import SwiftUI
+
+extension Color {
+    init(light: Color, dark: Color) {
+        #if canImport(UIKit)
+        self.init(UIColor { traits in
+            traits.userInterfaceStyle == .dark ? UIColor(dark) : UIColor(light)
+        })
+        #elseif canImport(AppKit)
+        self.init(nsColor: NSColor(name: nil) { appearance in
+            appearance.bestMatch(from: [.darkAqua, .vibrantDark]) != nil
+                ? NSColor(dark)
+                : NSColor(light)
+        })
+        #endif
+    }
+}
