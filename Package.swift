fd1666e5b8f8004160c2b4c50d98b96c0c2d81ec
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-06-26 11:01:26 +0200

    chore: Rename DesignSystem into ESDSDesignSystem

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "DesignSystem",
+    name: "ESDSDesignSystem",
     platforms: [.iOS(.v16), .macOS(.v13), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "DesignSystem",
+            name: "ESDSDesignSystem",
             targets: ["DesignSystem"]
         ),
     ],
