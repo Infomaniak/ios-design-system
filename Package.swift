aa0e1490fde0f2c3fa6f3ef65ee1dd91a2d7e005
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-06-26 11:04:22 +0200

    chore: Target name is ESDSDesignSystem

--- Package.swift
+++ Package.swift
@@ -10,18 +10,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ESDSDesignSystem",
-            targets: ["DesignSystem"]
+            targets: ["ESDSDesignSystem"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"
+            name: "ESDSDesignSystem",
+            path: "Sources/DesignSystem"
         ),
         .testTarget(
             name: "DesignSystemTests",
-            dependencies: ["DesignSystem"]
+            dependencies: ["ESDSDesignSystem"]
         ),
     ]
 )
