aee629f3eaf171fb7cdd24b965b45e321bef9f22
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-07-02 18:10:43 +0200

    feat: Add ESDSResources support

--- Package.swift
+++ Package.swift
@@ -12,12 +12,20 @@ let package = Package(
             name: "DesignSystem",
             targets: ["DesignSystem"]
         ),
+        .library(
+            name: "ESDSResources",
+            targets: ["ESDSResources"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"
+            name: "DesignSystem",
+            dependencies: ["ESDSResources"]
+        ),
+        .target(
+            name: "ESDSResources"
         ),
         .testTarget(
             name: "DesignSystemTests",
