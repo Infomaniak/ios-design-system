d242fe63945fb9f5dd71e90116faecb3f9517c06
lifaon74 <6762756+lifaon74@users.noreply.github.com>
2026-08-06 06:22:45 +0000

    chore: Apply swiftformat

--- Sources/Products/ESDSInfomaniak/Infomaniak+Icon.swift
+++ Sources/Products/ESDSInfomaniak/Infomaniak+Icon.swift
@@ -18,9 +18,9 @@
  Do not edit directly, this file was auto-generated.
  */
 
-import Foundation
 import ESDSFoundation
 import ESDSPrimitives
+import Foundation
 
 extension ESDSTheme.Icon {
     static let infomaniak = ESDSTheme.Icon(
